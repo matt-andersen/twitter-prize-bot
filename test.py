cd18577ff0566e802c134adeeccca934928de125
Matt Andersen <mattandersen0@gmail.com>
2019-09-16 14:45:09 +1000

    Update .gitignore

--- test.py
+++
@@ -1,8 +0,0 @@
-source_tweet_text = "This gift is hard to unwrap. Who can help me? rtes to win"
-source_tweet_text = source_tweet_text.lower()
-
-FORBIDDEN_WORDS = ["porn", "sex", "brazzers", "onlyfans", "#porn", "#sex", "horny", "sexy", "xxx", "ariana", "swift",
-                   "anal", "taylorswift", "comment", "tag", "reply", "cum", "full video"]
-
-if "rt" in source_tweet_text.split() or "retweet" in source_tweet_text.split() and "win" in source_tweet_text.split():
-    print(source_tweet_text.split())
